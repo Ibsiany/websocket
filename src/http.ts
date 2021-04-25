b19e0e8cefa50fbaefcc93fa5d406f7c1a60602d
ibsiany <ibsianyd@gmail.com>
2021-04-25 18:26:56 -0300

    nodeJS completo

---
+++ src/http.ts
@@ -0,0 +1,33 @@
+import express, { request } from 'express';
+import {createServer} from 'http';
+import {Server, Socket} from 'socket.io'
+import path from 'path';
+import "./database"
+import { routes } from './routes';
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+app.set("views", path.join(__dirname, '..', 'public'));
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html")
+
+app.get("/pages/client", (request, response) => {
+    return response.render("html/client.html")
+})
+
+app.get("/pages/admin", (request, response) => {
+    return response.render("html/admin.html")
+})
+
+const http = createServer(app); //criando protocolo http
+const io = new Server(http) // criando protocolo de websocket 
+
+io.on("connection", (socket: Socket) => {
+    console.log("Se conectou", socket.id)
+})
+
+app.use(express.json())
+app.use(routes)
+
+export {http, io}
